efd85fece4a464b2e4e64964dfa22d8da17a8971
Mario Bonsembiante <mario.bonsembiante@gmail.com>
2018-07-19 12:09:41 +0200

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,24 +1,17 @@
-from camera import *
+import camera as cam
+import 3dplot
 
-camera0 = camera(0)
-camera1 = camera(1)
-#try to load the camera matrix and other coefficient from the file.
-#If the file hasn't been written we try to create the parameters and then save them.
-camera0.loadCoefficient()
-camera0.undistorted
-camera1.loadCoefficient()
-if camera0.undistorted == False:
-    try:
-        camera0.createCoefficient()
-    except:
-        print("Error, camera images not present for camera number "+camera0.camera_number)
-if camera1.undistorted == False:
-    try:
-        camera1.createCoefficient()
-    except:
-        print("Error, camera images not present for camera number "+camera1.camera_number)
-camera0.poseEstimate()
-camera0.composeProjectionMatrix()
+def main():
+    camera1 = cam.camera(1, "/dev/video0")
+    camera2 = cam.camera(2, "/dev/video3")
+    if camera1.createCameraMatrixUndistort() == False:
+        return False
+    if camera2.createCameraMatrixUndistort() == False:
+        return False
+    camera1.showvideo()
+    camera2.showvideo()
+    x,y,z = camera1.TriangulateBallVideo(camera2, False)
+    3dplot.displayanimation(x, y, z)
 
-camera1.poseEstimate()
-camera1.composeProjectionMatrix()
+if __name__ == "__main__":
+    main()
